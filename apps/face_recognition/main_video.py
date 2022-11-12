91e3beccc46c3b31b3044ece8d389154a36646a6
Haider Zahoor <haider2090@gmail.com>
2022-11-12 15:38:17 +0500

    Added multi-user face recognition
    Multiple users are now correctly detected and identified. This update also allows you to set tolerance for face recognition camera.

--- apps/face_recognition/main_video.py
+++ apps/face_recognition/main_video.py
@@ -1,3 +1,5 @@
+import random
+
 import cv2
 from simple_facerec import SimpleFacerec
 import tkinter as tk
@@ -10,30 +12,44 @@ class FactoryFaceRecognition:
         self.sfr = SimpleFacerec()
         self.imagesPath = "images"
         self.sfr.load_encoding_images(self.imagesPath)
-        self.userName = ""
+        self.userName = []
         self.frameDelay = None
+        self.tolerance = 0.55
+        self.numberOfPeopleInFrame = 0
 
     def loadCamera(self):
-        self.cap = cv2.VideoCapture(2)
+        self.cap = cv2.VideoCapture(0)
 
     def checkForFace(self):
         ret, frame = self.cap.read()
         # Detect Faces
-        face_locations, face_names = self.sfr.detect_known_faces(frame)
-        for face_loc, name in zip(face_locations, face_names):
-            y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
-            cv2.imshow("Frame", frame)
-            if name == "Unknown":
-                self.addNewUser(frame)
-            else:
-                print("Face matched: " + name)
-                self.userName = name
-                self.cap.release()
-                cv2.destroyAllWindows()
-                return name
+        self.userName = []
+        face_locations, face_names = self.sfr.detect_known_faces(frame=frame, face_tolerance=self.tolerance)
+        self.numberOfPeopleInFrame = len(face_names)
 
-    def checkPersonName(self):
+        for face_loc, name in zip(face_locations, face_names):
+            if name is "Unknown" or name is "unknown":
+                name = "Unknown Individual"
+                cv2.imwrite("images/" + name + ".png", frame)
+                self.sfr.load_encoding_images(self.imagesPath)
+            # y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+            # cv2.imshow("Frame", frame)
+            self.userName.append(name)
+            # self.removeCamera()
         return self.userName
+
+    def checkPeopleInFrame(self):
+        return self.userName
+
+    def removeCamera(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+    def getTolerance(self):
+        return self.tolerance
+
+    def setTolerance(self, newTolerance):
+        self.tolerance = newTolerance
 
     def AddFrameDelay(self, delay):
         self.frameDelay = delay
@@ -47,96 +63,36 @@ class FactoryFaceRecognition:
                                                prompt="What's your Name?:")
         if self.userName is None:
             self.addNewUser(frame)
-            self.cap.release()
-            cv2.destroyAllWindows()
+            self.removeCamera()
             return True
         else:
             cv2.imwrite("images/" + self.userName + ".png", frame)
             self.sfr.load_encoding_images(self.imagesPath)
-            self.cap.release()
-            cv2.destroyAllWindows()
+            self.removeCamera()
             return False
 
 
-"""
 from main_video import FactoryFaceRecognition
-
+import msvcrt as m
 
 def main():
+    found_face = []
+
     faceRecognition = FactoryFaceRecognition()
     faceRecognition.loadCamera()
     print("Looking for people!")
-    while faceRecognition.checkPersonName() == "":
+    #    while faceRecognition.checkPersonName() == "":
+    #        found_face.append(faceRecognition.checkForFace())
+    userInput = 'X'
+    while userInput is not 'Q' or userInput is not 'q':
         faceRecognition.checkForFace()
+        if faceRecognition.numberOfPeopleInFrame > 0:
+            print(faceRecognition.checkPeopleInFrame())
+            #userInput = input("Press any key to continue...")
+            print("Press any key to continue")
+            userInput = m.getch()
 # ToDO: Add an option to checkForFace after X number of frames that can be manually assigned for ease of use for the
 #       developer.
-
-if __name__ == "__main__":
-    main()
-"""
-
-import face_recognition as fr
-import cv2
-import numpy as np
-import os
-class FactoryMultipleFacesRecognition:
-    def __init__(self):
-        self.cap = None
-        self.images_folder = 'images/'
-        self.load_image = None
-        self.target_image = None
-        self.target_encoding = None
-        self.face_recognition_tolerance = 0.6
-        self.faces_found = None
-
-    def loadCamera(self):
-        self.cap = cv2.VideoCapture(0)
-        self.cap.set(3, 640)  # set Width
-        self.cap.set(4, 480)  # set Height
-        ret, frame = self.cap.read()
-        # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-        rgb_small_frame = small_frame[:, :, ::-1]
-        self.target_image = fr.face_locations(rgb_small_frame)
-        self.target_encoding = fr.face_encodings(rgb_small_frame,self.target_image)
-        print(self.target_image)
-        print(self.target_encoding)
-
-    def encode_faces(self, folder):
-        list_people_encoding = []
-        for filename in os.listdir(folder):
-            known_image = fr.load_image_file(f'{folder}{filename}')
-            known_encoding = fr.face_encodings(known_image)[0]
-            list_people_encoding.append((known_encoding, filename))
-            print(list_people_encoding)
-        return list_people_encoding
-
-    def find_target_face(self):
-        # face_location = fr.face_locations(target_image)
-        faces_found = []
-        for person in self.encode_faces(self.images_folder):
-            encoded_face = person[0]
-            filename = person[1]
-            is_target_face = False
-            is_target_face = fr.compare_faces(encoded_face, self.target_encoding,
-                                              tolerance=self.face_recognition_tolerance)
-            for face in is_target_face:
-                if face:
-                    print("face found")
-                    index = filename.rfind(".")
-                    faces_found.append(filename[0:index])
-        return faces_found
-
-
-from main_video import FactoryMultipleFacesRecognition as multiFaceRecognition
-
-
-def main():
-    mf = multiFaceRecognition()
-    mf.loadCamera()
-    print(f"Faces found: {mf.find_target_face()}")
-    # render_image()
-
 
 if __name__ == "__main__":
     main()
